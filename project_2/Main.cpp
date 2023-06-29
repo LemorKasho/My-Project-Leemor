e6408080a9adaf7d8cf2bbe09fcc5ff151afaf5f
lemorkasho04 <lemorkasho04@gmail.com>
2023-06-29 22:57:03 +0300

    first editing

--- project_2/Main.cpp
+++ project_2/Main.cpp
@@ -1,107 +1,147 @@
 #include<iostream>
 using namespace std;
 
-void PrintNet(char arrayPRINT[9])
+void PrintNet(char ArrayPrintT[9])
 {
-	int count = 1;
+	int Count = 1;
 	for (int i = 0; i < 9; i++)
 	{
-		cout << " " << arrayPRINT[i] << " ";
-		if (count != 3 && count != 6 && count != 9)
+		cout << " " << ArrayPrintT[i] << " ";
+		if (Count != 3 && Count != 6 && Count != 9)
 			cout << "|";
-		if (count == 3 || count == 6)
+		if (Count == 3 || Count == 6)
 			cout << "\n" << "~~~~~~~~~~~" << "\n";
-		count++;
+		Count++;
 	}
 }
-void FillXO(int site, int CountPlay, char arrayPRINT[9])
+void FillXO(int Site, int CountPlay, char ArrayPrint[9])
 {
 	if (CountPlay % 2 == 0)
 	{
 
-		arrayPRINT[site-1] = 'O';
+		ArrayPrint[Site-1] = 'O';
 	}
 	else
 	{
-		arrayPRINT[site - 1] = 'X';
+		ArrayPrint[Site - 1] = 'X';
 	}
 }
-int checkCondition(char arrayPRINT[9] , int CountPlay)
+int CheckCondition(char ArrayPrint[9] , int CountPlay)
 {
-	char Win;
-	int x = 1;
-	int i = 0;
+	int WhoWin;
+	
 	if (CountPlay % 2 != 0)
-		Win = 'X';
+		WhoWin = 1;
 	else
-		Win = 'O';
-
-	if (arrayPRINT[i] == arrayPRINT[i + 1] && arrayPRINT[i + 1] == arrayPRINT[i + 2])
+		WhoWin = 2;
+	if (ArrayPrint[0] == ArrayPrint[1] && ArrayPrint[1] == ArrayPrint[2])
 	{
-		cout << "\n" << Win << "  is Winner";
+		return WhoWin;
 	}
-	else if (arrayPRINT[i + 3] == arrayPRINT[i + 4] && arrayPRINT[i + 4] == arrayPRINT[i + 5])
+	else if (ArrayPrint[3] == ArrayPrint[4] && ArrayPrint[4] == ArrayPrint[5])
 	{
-		cout << "\n" << Win << "  is Winner";
+		return WhoWin;
 	}
-	else if (arrayPRINT[i + 6] == arrayPRINT[i + 7] && arrayPRINT[i + 7] == arrayPRINT[i + 8])
+	else if (ArrayPrint[6] == ArrayPrint[7] && ArrayPrint[7] == ArrayPrint[8])
 	{
-		cout << "\n" << Win << "  is Winner";
+		return WhoWin;
 	}
-	else if (arrayPRINT[i + 0] == arrayPRINT[i + 3] && arrayPRINT[i + 3] == arrayPRINT[i + 6])
+	else if (ArrayPrint[0] == ArrayPrint[3] && ArrayPrint[3] == ArrayPrint[6])
 	{
-		cout << "\n" << Win << "  is Winner";
+		return WhoWin;
 	}
-	else if (arrayPRINT[i + 1] == arrayPRINT[i + 4] && arrayPRINT[i + 4] == arrayPRINT[i + 7])
+	else if (ArrayPrint[1] == ArrayPrint[4] && ArrayPrint[4] == ArrayPrint[7])
 	{
-		cout << "\n" << Win << "  is Winner";
+		return WhoWin;
 	}
-	else if (arrayPRINT[i + 2] == arrayPRINT[i + 5] && arrayPRINT[i + 5] == arrayPRINT[i + 8])
+	else if (ArrayPrint[2] == ArrayPrint[5] && ArrayPrint[5] == ArrayPrint[8])
 	{
-		cout << "\n" << Win << "  is Winner";
+		return WhoWin;
 	}
-	else if (arrayPRINT[i + 0] == arrayPRINT[i + 4] && arrayPRINT[i + 4] == arrayPRINT[i + 8])
+	else if (ArrayPrint[0] == ArrayPrint[4] && ArrayPrint[4] == ArrayPrint[8])
 	{
-		cout << "\n" << Win << "  is Winner";
+		return WhoWin;
 	}
-	else if (arrayPRINT[i + 2] == arrayPRINT[i + 4] && arrayPRINT[i + 4] == arrayPRINT[i + 6])
+	else if (ArrayPrint[2] == ArrayPrint[4] && ArrayPrint[4] == ArrayPrint[6])
 	{
-		cout << "\n" << Win << "  is Winner";
+		return WhoWin;
 	}
-	else
-		x = 0;
-	return x;
+	return WhoWin =0;
 }
-
 int main()
 {
-	cout << "X O Game";
-	char arrayPRINT[9] = {'1','2','3','4','5','6','7','8','9'};
-	PrintNet(arrayPRINT);
+	int AgainPlaying;
+	int CountXWin = 0;
+	int CountOWin = 0;
 
-	for (int CountPlay = 1; CountPlay < 10; CountPlay++)
+	do
 	{
-		int site;
-		AgainSite :
-		cout << "\n" << "please enter the site which you want from 1 to 9" << "\n";
-		cin >> site;
-		if (site < 1 || site > 9)
-			goto AgainSite;
-		if (arrayPRINT[site - 1] == 'X' || arrayPRINT[site - 1] == 'O')
-			goto AgainSite;
-		system("cls");
-		FillXO(site, CountPlay, arrayPRINT);
-		PrintNet(arrayPRINT);
-		if (CountPlay > 4)
+		cout << "X O Game" << "\n";
+		char ArrayPrint[9] = { '1','2','3','4','5','6','7','8','9' };
+		PrintNet(ArrayPrint);
+
+		for (int CountPlay = 1; CountPlay < 10; CountPlay++)
 		{
-			int x = checkCondition(arrayPRINT, CountPlay);
-			if (x == 1)
-				break;
+			if (CountPlay % 2 != 0)
+				cout << "\nX Is Playing";
+			else
+				cout << "\nO Is Playing";
+			int Site;
+		AgainSite:
+			cout << "\n" << "Please Enter The Site Which You Want From 1 To 9" << "\n";
+			try
+			{
+				if(!(cin >> Site))
+				{
+					cout << "\nYou Entered A String";
+					cin.clear();
+					cin.ignore(123,'\n');
+					goto AgainSite;
+				}
+				cin.ignore(123, ' ');
+				if (Site < 1 || Site > 9)
+					goto AgainSite;
+				if (ArrayPrint[Site - 1] == 'X' || ArrayPrint[Site - 1] == 'O')
+					throw"This Site Has Already Been entered \n";
+
+			}
+			catch (const char* ErrorMessage)
+			{
+				cout << "EXCEPTION : " << ErrorMessage;
+				goto AgainSite;
+			}
+			system("cls");
+			FillXO(Site, CountPlay, ArrayPrint);
+			PrintNet(ArrayPrint);
+			if (CountPlay > 4)
+			{
+				int x = CheckCondition(ArrayPrint, CountPlay);
+				if (x == 1)
+				{
+					CountXWin++;
+					break;
+				}
+				else if (x == 2)
+				{
+					CountOWin++;
+					break;
+				}
+			}
 		}
-		if (CountPlay == 9)
+		do
 		{
-			cout << "\n" << " no one is winner";
-		}
-	}
+			cout << "\nDo You Want Play Again. Enter 1 To Playing, Enter 0 To Stop.";
+			cin >> AgainPlaying;
+			system("cls");
+		} while (AgainPlaying != 1 && AgainPlaying != 0);
+
+	} while (AgainPlaying);
+	cout << "X :"<<CountXWin<<" "<<CountOWin<<": O";
+	if (CountXWin > CountOWin)
+		cout << "\nX Is Winner";
+	else if (CountOWin > CountXWin)
+		cout << "\nO Is Winner";
+	else
+		cout << "\nNo One Is Winner";
 	return 0;
 }
